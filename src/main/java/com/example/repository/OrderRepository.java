114b10589dc35893e3ff4979b81616e2e2eac3f1
ymnk10 <kseisaimin10@gmail.com>
2024-06-19 13:44:07 +0900

    1-7　注文をするのクラスファイル作成 フロント結合テスト前 curlコマンド実行確認前

---
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.example.repository;
+
+import com.example.domain.Set;
+
+import java.util.List;
+
+public class OrderRepository {
+    /**
+     * 注文情報の更新を行います.
+     * paymentMethodが1だったらstatusを1に、paymentMethodが2だったらstatusを2にする
+     *
+     */
+//    public void update(Order order) {
+//        String sql = "SELECT id,name,top_id,bottom_id,description,price,image_path FROM Sets ORDER BY id;";
+//        template.query(sql, ORDER_ROW_MAPPER);
+//    }
+}
