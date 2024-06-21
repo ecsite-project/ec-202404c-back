8d8509461b5d1219dfdcb6d8c71e5b17f5e17be7
ymnk10 <kseisaimin10@gmail.com>
2024-06-21 14:43:20 +0900

    2-9.注文の際にメールを送る(上級)(注文をする)

--- src/main/java/com/example/service/EmailService.java
+++ src/main/java/com/example/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import com.example.domain.Order;
+import com.example.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,6 +13,9 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         // メールの送信先(お客様)を指定
@@ -18,6 +23,14 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+
+
+    }
+
+    public Order getOrderInfo(Integer orderId){
+        // メールに記載するため、注文情報を取得
+        Order order = orderRepository.findById(orderId);
+        return order;
     }
 }
 
