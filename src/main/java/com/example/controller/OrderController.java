9895d6b219a2a8961119eca51152de13364c56f2
ymnk10 <kseisaimin10@gmail.com>
2024-06-21 12:15:52 +0900

    1-7　「注文をする」実装 フロント結合テスト前 成功情報をレスポンス

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,15 +1,21 @@
 package com.example.controller;
 
+import com.example.WebApiResponseObject;
 import com.example.domain.Address;
 import com.example.domain.Destination;
 import com.example.domain.Order;
+import com.example.domain.User;
 import com.example.request.OrderRequest;
 import com.example.service.OrderService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 注文をするコントローラです.
@@ -25,7 +31,7 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping("")
-    public void order(@RequestBody OrderRequest orderRequest){
+    public WebApiResponseObject order(@RequestBody OrderRequest orderRequest, HttpServletResponse response){
         Order order = new Order();
         Destination destination = new Destination();
         Address address = new Address();
@@ -33,5 +39,13 @@ public class OrderController {
         BeanUtils.copyProperties(orderRequest,destination);
         BeanUtils.copyProperties(orderRequest, address);
         orderService.order(order, destination, address);
+
+        // 成功情報をレスポンス
+        WebApiResponseObject webApiResponseObject = new WebApiResponseObject();
+        webApiResponseObject.setStatus("success");
+        webApiResponseObject.setMessage("OK.");
+        webApiResponseObject.setErrorCode("E-00");
+
+        return webApiResponseObject;
     }
 }
